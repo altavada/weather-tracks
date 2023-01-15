d18c1637d3b8b592143e9dabda8c984a738d1af7
altavada <117209509+altavada@users.noreply.github.com>
2023-01-15 16:23:12 -0500

    revised color coder, added comments and README

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# weather-tracks
-5-day weather app
+# WeatherTrax Forecast Finder
+
+## Description
+
+This project utilizes the Bootstrap framework and the fetch API method to handle user searches and return 5-day forecast data for a given location in the world by calling on the Open Weather Map server-side API. It also stores recent searches in the user's browser cache, and automatically creates and updates a list of recent searches. Numeric weather data is color-coded according to its values, to help the user easily visualize relative scale. Temperature (in Fahrenheit) and humidity (in percent humidity index) color-coding uses a base-10 scale and covers a normative range of 0-100, with additional temperature categories handling temperatures outside this range. Wind speed color-coding is based off the Beaufort wind force scale. As true daily highs and lows are not available via this API (a paid alternative exists from the same site that would fulfill this specification), future-date forecasts list results from 3PM each day, while current conditions are listed from the closest 3-hour forecast listing to the present time.
+
+User opens page for first time:
+![A first-time user opens the page](./Assets/images/sample.png)
+
+User performs first search (US location):
+![User performs first search](./Assets/images/sample2.png)
+
+User clicks "New Search" or reloads page, begins second search (non-US location). Recent Search history appears:
+![User clicks "New Search" or reloads page, and begins second search](./Assets/images/sample3.png)
+
+New search performed, Recent Search history persists:
+![New search performed, Recent Search history persists](./Assets/images/sample4.png)
+
+Recent Search item clicked. Search history and results updated:
+![Recent Search item clicked. Search history and results updated](./Assets/images/sample5.png)
+
+
+
+## Installation
+
+N/A
+
+## Usage
+
+When the user first visits the page, they are presented with a simple search bar. As indicated, the user should check the "Outside US" box to filter out results with a US state address (which entails different API formatting). When the user enters a valid location name and clicks "GO," the page returns current conditions and future conditions ranging from tomorrow to today-plus-5. If an invalid location is entered or weather data cannot be retrieved, the page does not change and a corresponding error message will display in the search bar. Following a successful search, the user can click "New Search" to return to the empty search bar and start again. When this is done, a "Recent Searches" table appears below the search bar. This table persists when new searches are performed or the page is reloaded, and will always display the most recent searches (max 5 at a time). When an item from this list is clicked, an up-to-date forecast for that location is returned. If a new search is performed and then an item from the recent history is clicked, the page immediately switches to that forecast and updates recent searches to include the last new search. If any search result is not returning correctly (returning an error state or an unintended location), try adding a two-letter state or country code to your search query separated by a comma. You can also try spelling out the state or country, also separated by a comma, if the abbreviation fails. At least one of these methods should sufficiently narrow down your search, provided the location exists in the API database. To view the deployed page, [click here](https://altavada.github.io/weather-tracks/).
+
+## Credits
+
+N/A
+
+## License
+
+Refer to license in the repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
 // Key: 0d53b40518548d72b4985f1cfd796f6a
 // Geocoder endpoint: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-var locationName = $('#locationname');
-var searchField = $('#searchfield');
-var searchHistory = {
-    destination: [],
-    region: []
-}
-
 $(function() {
+    var locationName = $('#locationname');
+    var searchField = $('#searchfield');
+    var searchHistory = {
+        destination: [],
+        region: []
+}
+    // Executes search
     $('#gobutton').click(initSearch);
 
+    // Resets page for new search
     $('#back').click(function() {
         getHistory();
         $('#searchbox').css('display', 'flex');
@@ -18,6 +19,7 @@ $(function() {
         searchField.val('');
     })
 
+    // Processes search request, fetches location coordinates, saves search if successful and if new search
     function initSearch(searchinput) {
         let fromHistory = true;
         let regionstatus = $('#checkbox').prop('checked')
@@ -42,7 +44,6 @@ $(function() {
                     searchField.val('ERROR -- Location not found');
                     return;
                 }
-                // console.log(data);
                 let country = data[0].country;
                 let state = data[0].state;
                 if (regionstatus == true && country == "US") {
@@ -66,6 +67,7 @@ $(function() {
             })
     }
 
+    // Fetches 5-day forecast data and writes to page, toggles page to display results
     function getResults(coord) {
         fetch('http://api.openweathermap.org/data/2.5/forecast?' + coord + '&appid=0d53b40518548d72b4985f1cfd796f6a&units=imperial')
         .then(function(response){
@@ -76,7 +78,6 @@ $(function() {
                 searchField.val('ERROR -- Forecast unavailable');
                 return;
             }
-            // console.log("Data object:", data);
             let list = data.list;
             let day = dayjs();
             let today = day.format('YYYY-MM-DD');
@@ -98,7 +99,6 @@ $(function() {
                     forecastData.icon.push(item.weather[0].icon);
                 }
             }
-            // console.log(forecastData);
             let weatherbox = $('#forecastwrapper').children();
             for (var i = 0; i < weatherbox.length; i++) {
                 let card = weatherbox.eq(i);
@@ -111,20 +111,22 @@ $(function() {
                 content.eq(0).text(forecastData.temp[i] + " (F)");
                 content.eq(1).text("Humidity: " + forecastData.hum[i] + "%");
                 content.eq(2).text("Wind: " + forecastData.wind[i] + "mph");
-                content.eq(3).text(forecastData.prec[i]);
-                content.eq(3).append($('<img>').addClass('forecast-icon').attr('src', `https://openweathermap.org/img/wn/${forecastData.icon[i]}.png`));
+                content.eq(3).text(forecastData.prec[i])
+                    .append($('<img>').addClass('forecast-icon').attr('src', `https://openweathermap.org/img/wn/${forecastData.icon[i]}.png`));
             }
             $('#searchbox').css('display', 'none');
             $('#resultswrapper').css('display', 'block');
         })
     }
 
+    // Saves new search to local storage
     function saveHistory() {
         searchHistory.destination.unshift(locationName.text());
         searchHistory.region.unshift($('#checkbox').prop('checked'));
         localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
     }
 
+    // gets stored data and creates a clickable list of recent searches, defines click handler to execute searches from recent history
     function getHistory() {
         $('#history-items').empty();
         let stored = JSON.parse(localStorage.getItem("searchHistory"));
@@ -157,37 +159,58 @@ $(function() {
         }
     }
 
+    // Color-codes weather data based on values
     function colorCoding(el, obj, x) {
         let temp = obj.temp[x];
         let tempcode;
-        if (temp <= 2) {
+        if (temp < 0) {
             tempcode = 'temp-a';
-        } else if (temp > 2 && temp <= 32) {
+        } else if (temp >= 0 && temp < 10) {
             tempcode = 'temp-b';
-        } else if (temp > 32 && temp <= 60) {
+        } else if (temp >= 10 && temp < 20) {
             tempcode = 'temp-c';
-        } else if (temp > 60 && temp <= 90) {
+        } else if (temp >= 20 && temp < 30) {
             tempcode = 'temp-d';
-        } else {
+        } else if (temp >= 30 && temp < 40) {
             tempcode = 'temp-e';
+        } else if (temp >= 40 && temp < 50) {
+            tempcode = 'temp-f';
+        } else if (temp >= 50 && temp < 60) {
+            tempcode = 'temp-g';
+        } else if (temp >= 60 && temp < 70) {
+            tempcode = 'temp-h';
+        } else if (temp >= 70 && temp < 80) {
+            tempcode = 'temp-i';
+        } else if (temp >= 80 && temp < 90) {
+            tempcode = 'temp-j';
+        } else if (temp >= 90 && temp < 100) {
+            tempcode = 'temp-k';
+        } else {
+            tempcode = 'temp-l';
         }
         el.eq(0).removeAttr('class').addClass('list-group-item ' + tempcode);
         let hum = obj.hum[x];
         let humcode;
-        if (hum <= 14.28) {
+        if (hum < 10) {
             humcode = 'hum-a';
-        } else if (hum > 14.28 && hum <= 28.57) {
+        } else if (hum >= 10 && hum < 20) {
             humcode = 'hum-b';
-        } else if (hum > 28.57 && hum <= 42.86) {
+        } else if (hum >= 20 && hum < 30) {
             humcode = 'hum-c';
-        } else if (hum > 42.86 && hum <= 57.14) {
+        } else if (hum >= 30 && hum < 40) {
             humcode = 'hum-d';
-        } else if (hum > 57.14 && hum <= 71.43) {
+        } else if (hum >= 40 && hum < 50) {
             humcode = 'hum-e';
-        } else if (hum > 71.43 && hum <= 85.71) {
+        } else if (hum >= 50 && hum < 60) {
             humcode = 'hum-f';
-        } else {
+        } else if (hum >= 60 && hum < 70) {
             humcode = 'hum-g';
+        } else if (hum >= 70 && hum < 80) {
+            humcode = 'hum-h';
+        } else if (hum >= 80 && hum < 90) {
+            humcode = 'hum-i';
+        } else {
+            humcode = 'hum-j';
         }
         el.eq(1).removeAttr('class').addClass('list-group-item ' + humcode);
         let wind =obj.wind[x];
@@ -222,5 +245,6 @@ $(function() {
         el.eq(2).removeAttr('class').addClass('list-group-item ' + windcode);
     }
 
+    // calls function when page loads, creates search history IF found
     getHistory();
 })
